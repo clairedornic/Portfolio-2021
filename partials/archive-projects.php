330f0e8883e98f63efcf82b8af5c7800b5317edd
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-07-09 22:47:53 +0200

    refactor: improve files structure

---
+++ partials/archive-projects.php
@@ -0,0 +1,59 @@
+<div data-scroll-section class="projets" id="projets">
+        <h2 data-scroll data-scroll-speed="2">Projets</h2>
+        <div class="gallery">
+          <a href="./tableaux-paris.html" class="gallery-item">  
+            <div class="gallery-item-img"> 
+              <img src="assets/img/tableaux-paris-icon.jpg" class="hover-link" alt="Projet de site e-commerce pour la marque Tableaux Paris">
+              <div class="emblem" id="projet-1">Développement web •</div>
+            </div>
+            <figcaption class="gallery-item-caption" data-scroll="" data-scroll-speed="1">
+              <h3 class="gallery-item-title">Tableaux Paris</h3>
+            </figcaption> 
+          </a>
+          <a href="./stocker-ranger.html" class="gallery-item">
+            <div class="gallery-item-img">
+              <img src="assets/img/stocker-ranger-icon.jpg" class="hover-link" alt="Projet de site vitrine pour la marque Stocker Ranger">
+              <div class="emblem" id="projet-2">Développement web •</div>
+            </div>
+            <figcaption class="gallery-item-caption" data-scroll="" data-scroll-speed="1">
+              <h3 class="gallery-item-title">Stocker Ranger</h3>
+            </figcaption>
+          </a>
+          <a href="./romain.html" class="gallery-item">
+            <div class="gallery-item-img">
+              <img src="assets/img/romain-icon.jpg" class="hover-link" alt="Projet de graphisme et de webdesign pour l'artiste Romain">
+              <div class="emblem" id="projet-3">Graphisme • Web Design •</div>
+            </div>
+            <figcaption class="gallery-item-caption" data-scroll="" data-scroll-speed="1">
+              <h3 class="gallery-item-title">Romain</h3>
+            </figcaption> 
+          </a>
+          <a href="./fem.html" class="gallery-item">
+            <div class="gallery-item-img">
+              <img src="assets/img/fem-icon.jpg" class="hover-link" alt="Projet de site web pour l'institut France Energies Marines">
+              <div class="emblem" id="projet-4">Développement web •</div>
+            </div>
+            <figcaption class="gallery-item-caption" data-scroll="" data-scroll-speed="1">
+              <h3 class="gallery-item-title">FEM</h3>
+            </figcaption>
+          </a> 
+          <a href="./serial-killer.html" class="gallery-item">
+            <div class="gallery-item-img">
+              <img src="assets/img/serial-killer-icon.jpg" class="hover-link" alt="Projet de webdocumentaire sur le thème des serials killers">
+              <div class="emblem" id="projet-5">Développement web •</div>
+            </div> 
+            <figcaption class="gallery-item-caption" data-scroll="" data-scroll-speed="1">
+              <h3 class="gallery-item-title">Serial Killer</h3>
+            </figcaption>  
+          </a>  
+          <a href="./atelier.html" class="gallery-item">
+            <div class="gallery-item-img">
+              <img src="assets/img/atelier-cm-icon.jpg" class="hover-link" alt="Projet de site vitrine pour l'Aterlier C+M">
+              <div class="emblem" id="projet-6">Développement web •</div>
+            </div> 
+            <figcaption class="gallery-item-caption" data-scroll="" data-scroll-speed="1">
+              <h3 class="gallery-item-title">Atelier C + M</h3>
+            </figcaption> 
+          </a>
+        </div>
+      </div> 
---
+++ partials/contact.php
@@ -0,0 +1,18 @@
+<div data-scroll-section class="contact" id="contact">
+    <div class="contact-oppo">
+        <h2>Contact</h2>
+        <div class="opportunites">
+        <h3 id="open-oppo" data-scroll data-scroll-speed="1">Ouverte aux opportunités</h3>
+        <div class="link-social">
+            <a href="https://www.linkedin.com/in/claire-dornic-32900215a/" target="_blank" class="hover-link">Linkedin</a>
+            <a href="https://twitter.com/Claire_dornic" target="_blank" class="hover-link">Twitter</a>
+            <a href="./assets/pdf/CV_claire_dornic.pdf" target="_blank" class="hover-link">CV</a>
+        </div>
+        </div>
+    </div>
+    <div class="infos-contact">
+        <p>Pour toutes informations, <br>
+        ou si vous souhaitez discuter</p>
+        <a href="mailto:dornic.claire@gmail.com" class="hover-link">dornic.claire@gmail.com</a>
+    </div>
+</div>
