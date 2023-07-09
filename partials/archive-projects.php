394f50e4c85bfe8617d1726fe957c2b0690b5b9e
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-07-09 23:39:08 +0200

    feat: create a loop on projects

--- partials/archive-projects.php
+++ partials/archive-projects.php
@@ -1,59 +1,22 @@
 <div data-scroll-section class="projets" id="projets">
         <h2 data-scroll data-scroll-speed="2">Projets</h2>
         <div class="gallery">
-          <a href="./tableaux-paris.html" class="gallery-item">  
+        <?php
+        $projects = json_decode( file_get_contents( './content/projects.json' ), true );
+        foreach ( $projects as $project ) {
+           ?>
+            <a href="./project?name=<?php echo $project['slug'] ?>" class="gallery-item">  
             <div class="gallery-item-img"> 
-              <img src="assets/img/tableaux-paris-icon.jpg" class="hover-link" alt="Projet de site e-commerce pour la marque Tableaux Paris">
-              <div class="emblem" id="projet-1">Développement web •</div>
+              <img src="./assets/img/<?php echo $project['slug'] ?>-icon.jpg" class="hover-link" alt="">
+              <div class="emblem" id="projet-1"><?php echo $project['type'] ?> •</div>
             </div>
             <figcaption class="gallery-item-caption" data-scroll="" data-scroll-speed="1">
-              <h3 class="gallery-item-title">Tableaux Paris</h3>
+              <h3 class="gallery-item-title"><?php echo $project['title'] ?></h3>
             </figcaption> 
           </a>
-          <a href="./stocker-ranger.html" class="gallery-item">
-            <div class="gallery-item-img">
-              <img src="assets/img/stocker-ranger-icon.jpg" class="hover-link" alt="Projet de site vitrine pour la marque Stocker Ranger">
-              <div class="emblem" id="projet-2">Développement web •</div>
-            </div>
-            <figcaption class="gallery-item-caption" data-scroll="" data-scroll-speed="1">
-              <h3 class="gallery-item-title">Stocker Ranger</h3>
-            </figcaption>
-          </a>
-          <a href="./romain.html" class="gallery-item">
-            <div class="gallery-item-img">
-              <img src="assets/img/romain-icon.jpg" class="hover-link" alt="Projet de graphisme et de webdesign pour l'artiste Romain">
-              <div class="emblem" id="projet-3">Graphisme • Web Design •</div>
-            </div>
-            <figcaption class="gallery-item-caption" data-scroll="" data-scroll-speed="1">
-              <h3 class="gallery-item-title">Romain</h3>
-            </figcaption> 
-          </a>
-          <a href="./fem.html" class="gallery-item">
-            <div class="gallery-item-img">
-              <img src="assets/img/fem-icon.jpg" class="hover-link" alt="Projet de site web pour l'institut France Energies Marines">
-              <div class="emblem" id="projet-4">Développement web •</div>
-            </div>
-            <figcaption class="gallery-item-caption" data-scroll="" data-scroll-speed="1">
-              <h3 class="gallery-item-title">FEM</h3>
-            </figcaption>
-          </a> 
-          <a href="./serial-killer.html" class="gallery-item">
-            <div class="gallery-item-img">
-              <img src="assets/img/serial-killer-icon.jpg" class="hover-link" alt="Projet de webdocumentaire sur le thème des serials killers">
-              <div class="emblem" id="projet-5">Développement web •</div>
-            </div> 
-            <figcaption class="gallery-item-caption" data-scroll="" data-scroll-speed="1">
-              <h3 class="gallery-item-title">Serial Killer</h3>
-            </figcaption>  
-          </a>  
-          <a href="./atelier.html" class="gallery-item">
-            <div class="gallery-item-img">
-              <img src="assets/img/atelier-cm-icon.jpg" class="hover-link" alt="Projet de site vitrine pour l'Aterlier C+M">
-              <div class="emblem" id="projet-6">Développement web •</div>
-            </div> 
-            <figcaption class="gallery-item-caption" data-scroll="" data-scroll-speed="1">
-              <h3 class="gallery-item-title">Atelier C + M</h3>
-            </figcaption> 
-          </a>
+           <?php
+        }
+        ?>
+          
         </div>
       </div> 
